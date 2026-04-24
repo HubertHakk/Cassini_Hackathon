3b40e076d12071b651f2ef505f0a2ae67b948eda
Hubert Hakk <h.hakk@student.tudelft.nl>
2026-04-24 20:28:28 +0200

    Initial Imports and filepaths

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
+import pydeck as pdk
+import geopandas as gpd
+import json
+import sqlite3
+import pandas as pd
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.set_page_config(page_title="DHSegura Map", layout="wide")
+st.title("DHSegura GeoJSON Map")
+
+### Setting paths to the relevant files
+geojson_path = "DHSegura.geojson"
+gpkg_path = "well_datapoints.gpkg"
